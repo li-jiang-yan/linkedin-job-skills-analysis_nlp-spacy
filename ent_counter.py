4f8e45f1fb359e1315b2bdd56384d183c5ebd429
li-jiang-yan <professional.jiangyan_li@outlook.com>
2026-01-06 01:56:32 +0800

    Added counting of entities

---
+++ ent_counter.py
@@ -0,0 +1,21 @@
+"""
+Counts entity text occurrence from ent_filter_.spacy
+"""
+from collections import Counter
+import json
+from operator import attrgetter
+import spacy
+from spacy.tokens import DocBin
+
+def ent_texts(d):
+    """Returns a set containing the text of entities in d"""
+    return set(map(attrgetter("text"), d.ents))
+
+nlp = spacy.load("en_core_web_lg")
+doc_bin = DocBin().from_disk("ent_filter_.spacy")
+docs = doc_bin.get_docs(nlp.vocab)
+c = Counter()
+for doc in docs:
+    c.update(ent_texts(doc))
+with open("ent_counter.json", "w", encoding="utf-8") as f:
+    json.dump(c.most_common(), f, indent=4)
